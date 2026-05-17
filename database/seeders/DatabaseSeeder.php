4c19d67646e758084e27488695a766898e9d825a
kaeto <andivir011@gmail.com>
2026-05-17 18:11:09 +0700

    clean

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,32 +2,10 @@
 
 namespace Database\Seeders;
 
-<<<<<<< HEAD
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-=======
->>>>>>> 1ddd39dd85bf8fe9b89d4203652d50b98cac69ae
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-<<<<<<< HEAD
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
-}
-=======
     public function run()
     {
         $this->call([
@@ -39,4 +17,3 @@ class DatabaseSeeder extends Seeder
         ]);
     }
 }
->>>>>>> 1ddd39dd85bf8fe9b89d4203652d50b98cac69ae
